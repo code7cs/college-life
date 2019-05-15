df16a3f9410231ca8164bb5193d4a20b9b7df828
hanfanw <hanfanw@gmail.com>
2019-05-15 01:58:14 -0400

    no message

---
+++ src/components/Queryarticle.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import { CardGroup } from "react-bootstrap";
+// import Club from "./Club";
+
+class QueryArticles extends Component {
+    render() {
+        const { clubs } = this.props;
+        return (
+            <div>
+                {/* TODO: one line display three cards, not done yet */}
+                <CardGroup className="container col-md-12">
+                    {clubs.map(club => (
+                        <Club key={club.id} club={club} />
+                    ))}
+                </CardGroup>
+            </div>
+        );
+    }
+}
+
+export default QueryArticles;
